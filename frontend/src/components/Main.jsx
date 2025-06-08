485d58d5f0a01237a443d2a47035a51a2a611ac4
ahmad-kaif <heyahmad611@gmail.com>
2025-06-08 13:34:39 +0530

    updated deployed link

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_URL = "http://localhost:3001";
+const API_URL = "https://uwodwebbackend.onrender.com";
 
 const Main = () => {
   const [selectedImage, setSelectedImage] = useState(null);
